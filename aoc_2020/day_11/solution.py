8de5d9c47e1ab475deb443161feca1668b3499d4
Jan-Benedikt <jan.jagusch@gmail.com>
2020-12-12 08:59:15 +0100

    sovle part two (ugly, but working)

--- aoc_2020/day_11/solution.py
+++ aoc_2020/day_11/solution.py
@@ -2,8 +2,27 @@
 
 """
 
+from itertools import count
+
 _DIRS = (-1, 0, 1)
 _DIRECTIONS = tuple((x, y) for x in _DIRS for y in _DIRS if x or y)
+
+
+def look_into_direction(seats, pos, direction, max_horizon):
+    for multiplier in count(start=1):
+        if max_horizon is not None and multiplier > max_horizon:
+            break
+        vector = direction[0] * multiplier, direction[1] * multiplier
+        new_pos = pos[0] + vector[0], pos[1] + vector[1]
+        if any(val < 0 for val in new_pos):
+            break
+        try:
+            seat = seats[new_pos[1]][new_pos[0]]
+        except IndexError:
+            break
+        if seat is not None:
+            return seat
+    return None
 
 
 def _format_seat(seat):
@@ -42,24 +61,16 @@ def read_input():
         )
 
 
-def gen_neighbors(seats, pos):
-    x, y = pos
+def gen_neighbors(seats, pos, max_horizon):
     for direction in _DIRECTIONS:
-        dx, dy = direction
-        nx, ny = x + dx, y + dy
-        if any(val < 0 for val in (nx, ny)):
-            continue
-        try:
-            yield seats[ny][nx]
-        except IndexError:
-            pass
+        yield look_into_direction(seats, pos, direction, max_horizon)
 
 
 def _apply_rule(seats, pos):
     x, y = pos
     if (seat := seats[y][x]) is None:
         return None
-    n_occupied = sum(neighbor for neighbor in gen_neighbors(seats, pos) if neighbor)
+    n_occupied = sum(neighbor for neighbor in gen_neighbors(seats, pos, 1) if neighbor)
     if not seat and not n_occupied:
         return True
     if seat and n_occupied >= 4:
@@ -67,21 +78,55 @@ def _apply_rule(seats, pos):
     return seat
 
 
-def calc_new_seats(seats, shape):
+def rule_1(seat, neighbors):
+    if seat is None:
+        return None
+    n_occupied = sum(neighbor for neighbor in neighbors if neighbor)
+    if not seat and not n_occupied:
+        return True
+    if seat and n_occupied >= 4:
+        return False
+    return seat
+
+
+def rule_2(seat, neighbors):
+    if seat is None:
+        return None
+    n_occupied = sum(neighbor for neighbor in neighbors if neighbor)
+    if not seat and not n_occupied:
+        return True
+    if seat and n_occupied >= 5:
+        return False
+    return seat
+
+
+def calc_new_seats(seats, shape, max_horizon, rule):
     width, height = shape
     return tuple(
-        tuple(_apply_rule(seats, (x, y)) for x in range(width)) for y in range(height)
+        tuple(
+            (rule(seats[y][x], gen_neighbors(seats, (x, y), max_horizon)))
+            for x in range(width)
+        )
+        for y in range(height)
     )
 
 
-def solve_part_one(seats):
+def evolve(seats, max_horizon, rule):
     shape = len(seats[0]), len(seats)
     old_seats = None
     new_seats = seats
     while old_seats != new_seats:
         old_seats = new_seats
-        new_seats = calc_new_seats(new_seats, shape)
+        new_seats = calc_new_seats(new_seats, shape, max_horizon, rule)
     return sum(seat for row in new_seats for seat in row if seat)
+
+
+def solve_part_one(seats):
+    return evolve(seats, 1, rule_1)
+
+
+def solve_part_two(seats):
+    return evolve(seats, None, rule_2)
 
 
 if __name__ == "__main__":
@@ -89,3 +134,5 @@ if __name__ == "__main__":
     solution_1 = solve_part_one(seats)
     assert solution_1 == 2254
     print(f"The solution to part 1 is '{solution_1}'.")
+    solution_2 = solve_part_two(seats)
+    print(f"The solution to part 2 is '{solution_2}'.")
