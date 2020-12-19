d7ba467530506941203c6cf5022dfea99d0f7c8c
janjagusch <jan.jagusch@gmail.com>
2020-12-19 17:38:48 +0100

    solve part 2 (ugly, but working)

--- aoc_2020/day_19/solution.py
+++ aoc_2020/day_19/solution.py
@@ -3,12 +3,6 @@
 """
 
 
-class RuleError(Exception):
-    """
-    Error that is thrown when applying rule fails.
-    """
-
-
 def _process_messages(messages):
     return tuple(line for line in messages.split())
 
@@ -63,8 +57,56 @@ def solve_part_one(messages, rules):
     return sum(solve(message, rules, "0") for message in messages)
 
 
+def replace_rules_8_and_11(rules):
+    """
+    8: 42 | 42 8
+    11: 42 31 | 42 11 31
+    """
+    rules["8"] = (("42",), ("42", "8"))
+    rules["11"] = (
+        (
+            "42",
+            "31",
+        ),
+        ("42", "11", "31"),
+    )
+
+
+def _solve_part_two(message, rules):
+    s = (message,)
+    i_42 = 0
+    while True:
+        new_s = tuple(solve_expressions(s, rules["42"], rules))
+        if not new_s:
+            if i_42 < 2:
+                return False
+            break
+        else:
+            s = new_s
+            i_42 += 1
+    i_31 = 0
+    while True:
+        new_s = tuple(solve_expressions(s, rules["31"], rules))
+        if not new_s:
+            if i_31 < 1:
+                return False
+            break
+        else:
+            s = new_s
+            i_31 += 1
+    return "" in s and i_42 > i_31
+
+
+def solve_part_two(messages, rules):
+    return sum(_solve_part_two(message, rules) for message in messages)
+
+
 if __name__ == "__main__":
     rules, messages = read_input()
     solution_1 = solve_part_one(messages, rules)
-    assert solution_1 == 285
+    # assert solution_1 == 285
     print(f"The solution to part 1 is '{solution_1}'.")
+    replace_rules_8_and_11(rules)
+    solution_2 = solve_part_two(messages, rules)
+    assert solution_2 == 412
+    print(f"The solution to part 2 is '{solution_2}'.")
