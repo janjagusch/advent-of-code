479a7eb28fbb4234e6f34b66b6b10f96fcd9e5c9
janjagusch <jan.jagusch@gmail.com>
2020-12-23 17:51:10 +0100

    format

--- aoc_2020/day_23/solution.py
+++ aoc_2020/day_23/solution.py
@@ -17,22 +17,21 @@
 
 # ## Day 1
 
-from itertools import count, islice
 from array import array
+from itertools import count, islice
 from math import prod
 from typing import Iterable
 
 
 # +
 class LinkedNode:
-    
     def __init__(self, data, next_node=None):
         self.data = data
         self.next_node = next_node
-        
+
     def __repr__(self):
         return f"{self.__class__.__name__}(data={self.data})"
-    
+
     def gen_next_n_nodes(self, n):
         node = self.next_node
         yield node
@@ -42,34 +41,36 @@ class LinkedNode:
                 raise IndexError(n)
             yield node
 
+
 class LinkedList:
     """
     A linked list with O(1) lookup for individual nodes.
+    Loops between the last and the first node.
     """
-    
+
     def __init__(self, nodes):
         self._head = nodes[0]
         self._nodes_map = {node.data: node for node in nodes}
         for node, next_node in zip(nodes[:-1], nodes[1:]):
             node.next_node = next_node
         nodes[-1].next_node = nodes[0]
-            
+
     @property
     def head(self):
         return self._head
-    
+
     def __getitem__(self, index) -> LinkedNode:
         return self._nodes_map[index]
-    
+
     def __iter__(self):
         node = self._head
         while node is not None:
             yield node
             node = node.next_node
-            
+
     def __len__(self):
         return len(self._nodes_map)
-    
+
     @property
     def data(self):
         visited_nodes = set()
@@ -80,26 +81,28 @@ class LinkedList:
             data.append(node)
             visited_nodes.add(node)
         return data
-            
+
     def __repr__(self):
         return f"{self.__class__.__name__}({self.data})"
-    
+
     def __str__(self):
-        return ''.join([str(node.data) for node in self.data])
+        return "".join([str(node.data) for node in self.data])
 
 
 # -
 
-CUP_LABELLING = "389125467"
+CUP_LABELLING = "368195742"
 
 nodes = tuple(LinkedNode(int(val) - 1) for val in CUP_LABELLING)
 cups = LinkedList(nodes)
 
 
-def find_destination(current: LinkedNode, pick_ups: Iterable[LinkedNode], cups: LinkedList) -> LinkedNode:
+def find_destination(
+    current: LinkedNode, pick_ups: Iterable[LinkedNode], cups: LinkedList
+) -> LinkedNode:
     """
     The crab selects a destination cup: the cup with a label equal
-    to the current cup's label minus one. If this would select 
+    to the current cup's label minus one. If this would select
     ne of the cups that was just picked up, the crab will keep
     subtracting one until it finds a cup that wasn't just picked up.
     If at any point in this process the value goes below the lowest
@@ -113,7 +116,7 @@ def find_destination(current: LinkedNode, pick_ups: Iterable[LinkedNode], cups: 
     raise ValueError(current, pick_ups)
 
 
-def play_game(cups, n_pick_ups=3):  
+def play_game(cups, n_pick_ups=3):
     for current in cups:
         pick_ups = tuple(current.gen_next_n_nodes(n_pick_ups))
         destination = find_destination(current, pick_ups, cups)
@@ -123,12 +126,12 @@ def play_game(cups, n_pick_ups=3):
         yield cups
 
 
-def solve(cups, n_moves):
-    cups = tuple(tqdm(islice(play_game(cups), n_moves)))[0]
+def solve_part_one(cups):
+    cups = tuple(islice(play_game(cups), 100))[0]
     return "".join(str(val.data + 1) for val in cups[0].gen_next_n_nodes(8))
 
 
-solution_1 = solve(cups, 100)
+solution_1 = solve_part_one(cups)
 
 assert solution_1 == "95648732"
 
@@ -136,15 +139,20 @@ print(f"The solution to part 1 is '{solution_1}'.")
 
 # ## Part 2
 
-nodes = tuple(LinkedNode(int(val) - 1) for val in CUP_LABELLING) + tuple((LinkedNode(val) for val in range(max(node.data for node in nodes) + 1, 1_000_000)))
+nodes = tuple(LinkedNode(int(val) - 1) for val in CUP_LABELLING)
+nodes += tuple(
+    (LinkedNode(val) for val in range(max(node.data for node in nodes) + 1, 1_000_000))
+)
 cups = LinkedList(nodes)
 
 
 def solve_part_two(cups):
-    cups = tuple(tqdm(islice(play_game(cups), 10_000_000)))[0]
+    cups = tuple(islice(play_game(cups), 10_000_000))[0]
     return prod(node.data + 1 for node in cups[0].gen_next_n_nodes(2))
 
 
 solution_2 = solve_part_two(cups)
 
-assert solution_2 == 149245887792
+assert solution_2 == 192515314252
+
+print(f"The solution to part 2 is '{solution_2}'.")
