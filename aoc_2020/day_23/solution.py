a7a1639caaf1d346b03058a8cab7ab7750b578c8
janjagusch <jan.jagusch@gmail.com>
2020-12-23 17:45:49 +0100

    solve test case for part 2

--- aoc_2020/day_23/solution.py
+++ aoc_2020/day_23/solution.py
@@ -19,41 +19,85 @@
 
 from itertools import count, islice
 from array import array
-
-CUP_LABELLING = "389125467"
-
-
-def process_cup_labelling(cup_labelling):
-    return [int(val) - 1 for val in cup_labelling]
-
-
-def wrapped_indexing(iterable, start, end):
-    actual_end = end % len(iterable)
-    if actual_end <= start:
-        return iterable[start:] + iterable[:actual_end]
-    return iterable[start:actual_end]
-
-
-circle = process_cup_labelling(CUP_LABELLING)
+from math import prod
 
 
 # +
-def gen_current_position(cup_len):
-    for i in count():
-        yield i % cup_len
-        
-        
-def pick_up(circle, current_index, n_pick_ups):
-    """
-    The crab picks up the three cups that are immediately
-    clockwise of the current cup. They are removed from the circle.
-    """
-    pick_ups = wrapped_indexing(circle, current_index, current_index + n_pick_ups)
-    circle = [val for val in circle if val not in pick_ups]
-    return circle, pick_ups   
+class LinkedNode:
     
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
         
-def find_destination(current, possible_cups, cup_len):
+    def __repr__(self):
+        return f"{self.__class__.__name__}(data={self.data})"
+    
+    def gen_next_n_nodes(self, n):
+        node = self.next_node
+        yield node
+        for _ in range(1, n):
+            node = node.next_node
+            if not node:
+                raise IndexError(n)
+            yield node
+
+class LinkedList:
+    """
+    A linked list with O(1) lookup for individual nodes.
+    """
+    
+    def __init__(self, nodes):
+        self._head = nodes[0]
+        self._nodes_map = {node.data: node for node in nodes}
+        for node, next_node in zip(nodes[:-1], nodes[1:]):
+            node.next_node = next_node
+        nodes[-1].next_node = nodes[0]
+            
+    @property
+    def head(self):
+        return self._head
+    
+    def __getitem__(self, index) -> LinkedNode:
+        return self._nodes_map[index]
+    
+    def __iter__(self):
+        node = self._head
+        while node is not None:
+            yield node
+            node = node.next_node
+            
+    def __len__(self):
+        return len(self._nodes_map)
+    
+    @property
+    def data(self):
+        visited_nodes = set()
+        data = []
+        for node in self:
+            if node in visited_nodes:
+                return data
+            data.append(node)
+            visited_nodes.add(node)
+        return data
+            
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.data})"
+    
+    def __str__(self):
+        return ''.join([str(node.data) for node in self.data])
+
+
+# -
+
+CUP_LABELLING = "389125467"
+
+nodes = tuple(LinkedNode(int(val) - 1) for val in CUP_LABELLING)
+cups = LinkedList(nodes)
+
+from typing import Iterable
+
+
+def find_destination(current: LinkedNode, pick_ups: Iterable[LinkedNode], cups: LinkedList) -> LinkedNode:
     """
     The crab selects a destination cup: the cup with a label equal
     to the current cup's label minus one. If this would select 
@@ -63,68 +107,36 @@ def find_destination(current, possible_cups, cup_len):
     value on any cup's label, it wraps around to the highest value
     on any cup's label instead.
     """
-    for i in range(1, cup_len):
-        destination = (current - i) % cup_len
-        if destination in possible_cups:
+    for i in range(1, len(cups)):
+        destination = cups[(current.data - i) % len(cups)]
+        if destination not in pick_ups:
             return destination
-    raise ValueError(current, cup_len, possible_cup)
-    
-def place_cups(circle, pick_ups, destination_index):
-    """
-    The crab places the cups it just picked up so that they are
-    immediately clockwise of the destination cup.
-    They keep the same order as when they were picked up.
-    
-    Alters circle inplace!
-    """
-    circle[destination_index:destination_index] = pick_ups
-
-def reorder_circle(circle, current, current_index):
-    """
-    Current needs to stay at current_index.
-    """
-    start_index = circle.index(current) - current_index
-    return circle[start_index:] + circle[:start_index]
+    raise ValueError(current, pick_ups)
 
 
-def relabel_cups(circle):
-    return [label + 1 for label in circle]
+from copy import deepcopy
 
 
-# -
-
-def play_game(circle, n_pick_ups=3):
-    circle = list(val for val in circle)
-    circle_len = len(circle)
-    for current_index in gen_current_position(circle_len):
-        # print(circle)
-        # print(f"cups: {' '.join(str(val) for val in circle)}")
-        current = circle[current_index]
-        circle, pick_ups = pick_up(circle, current_index + 1, n_pick_ups)
-        # print(f"current: {current}")
-        # print(f"pick up: {pick_ups}")
-        destination = find_destination(current, circle, circle_len)
-        # print(f"destination: {destination}")
-        destination_index = circle.index(destination)
-        # print(f"sub circle: {circle}")
-        place_cups(circle, pick_ups, destination_index + 1)
-        # print(f"new circle: {circle}")
-        circle = reorder_circle(circle, current, current_index)
-        # print("")
-        yield circle
+def play_game(cups, n_pick_ups=3):  
+    for current in cups:
+        # print(cups)
+        # print(current)
+        pick_ups = tuple(current.gen_next_n_nodes(n_pick_ups))
+        # print(pick_ups)
+        destination = find_destination(current, pick_ups, cups)
+        # print(destination)
+        current.next_node = pick_ups[-1].next_node
+        pick_ups[-1].next_node = destination.next_node
+        destination.next_node = pick_ups[0]
+        yield cups
 
 
-def solve_part_one(circle):
-    circle = tuple(islice(play_game(circle), 100))[-1]
-    circle = relabel_cups(circle)
-    start_index = circle.index(1)
-    circle = (circle[start_index:] + circle[:start_index])[1:]
-    return "".join(str(val) for val in circle)
+def solve(cups, n_moves):
+    cups = tuple(tqdm(islice(play_game(cups), n_moves)))[0]
+    return "".join(str(val.data + 1) for val in cups[0].gen_next_n_nodes(8))
 
 
-solution_1 = solve_part_one(circle)
-
-solution_1
+solution_1 = solve(cups, 100)
 
 # +
 # assert solution_1 == "95648732"
@@ -134,93 +146,17 @@ print(f"The solution to part 1 is '{solution_1}'.")
 
 # ## Part 2
 
-circle2 = array("I", circle + list(range(max(circle) + 1, 1_000_000)))
+nodes = tuple(LinkedNode(int(val) - 1) for val in CUP_LABELLING) + tuple((LinkedNode(val) for val in range(max(node.data for node in nodes) + 1, 1_000_000)))
+cups = LinkedList(nodes)
 
 
-# +
-def pick_up(circle, current_index, n_pick_ups):
-    """
-    The crab picks up the three cups that are immediately
-    clockwise of the current cup. They are removed from the circle.
-    """
-    end_index = current_index + n_pick_ups
-    end_index %= len(circle)
-    if end_index <= current_index:
-        pick_ups = circle[current_index:] + circle[:end_index]
-        circle = circle[end_index:current_index]
-    else:
-        pick_ups = circle[current_index:end_index]
-        circle = circle[:current_index] + circle[end_index:]
-    return circle, pick_ups  
-
-def find_destination(current, possible_cups, pick_ups, cup_len):
-    """
-    The crab selects a destination cup: the cup with a label equal
-    to the current cup's label minus one. If this would select 
-    ne of the cups that was just picked up, the crab will keep
-    subtracting one until it finds a cup that wasn't just picked up.
-    If at any point in this process the value goes below the lowest
-    value on any cup's label, it wraps around to the highest value
-    on any cup's label instead.
-    """
-    for i in range(1, cup_len):
-        destination = (current - i) % cup_len
-        if destination not in pick_ups:
-            return destination
-    raise ValueError(current, cup_len, possible_cup)
+def solve_part_two(cups):
+    cups = tuple(tqdm(islice(play_game(cups), 10_000_000)))[0]
+    return prod(node.data + 1 for node in cups[0].gen_next_n_nodes(2))
 
 
-# -
+solution_2 = solve_part_two(cups)
 
-def play_game(circle, n_pick_ups=3):
-    circle = array("I", circle)
-    circle_len = len(circle)
-    for current_index in gen_current_position(circle_len):
-        # print(circle)
-        # print(f"cups: {' '.join(str(val) for val in circle)}")
-        current = circle[current_index]
-        circle, pick_ups = pick_up(circle, current_index + 1, n_pick_ups)
-        # print(f"current: {current}")
-        # print(f"pick up: {pick_ups}")
-        destination = find_destination(current, circle, pick_ups, circle_len)
-        # print(f"destination: {destination}")
-        destination_index = circle.index(destination)
-        # print(f"sub circle: {circle}")
-        place_cups(circle, pick_ups, destination_index + 1)
-        # print(f"new circle: {circle}")
-        circle = reorder_circle(circle, current, current_index)
-        # print("")
-        yield circle
+solution_2
 
-
-solution_1 = solve_part_one(circle)
-
-solution_1
-
-assert solution_1 == "67384529"
-
-
-def solve_part_two(circle):
-    circle = tuple(islice(play_game(circle), 10_000_000))[-1]
-    circle = relabel_cups(circle)
-    start_index = circle.index(1)
-    circle = (circle[start_index:] + circle[:start_index])[1:]
-    return "".join(str(val) for val in circle)
-
-
-# +
-# solve_part_two(circle)
-# -
-
-for i, state in enumerate(islice(play_game(circle2), 10_000_000)):
-    if not i % 100:
-        print(i)
-    
-
-
-
-a = array("H", [1, 2, 3])
-
-a[0]
-
-
+assert solution_2 == 149245887792
